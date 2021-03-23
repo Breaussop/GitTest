5cd6734984c8a3fcbdf0bf273b603ccc3ad4f1b4
kennethAdams <kennethjadams95@gmail.com>
2021-03-23 09:57:59 -0400

    Initial Commit

---
+++ gittest/gittestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittestApp.swift
+//  gittest
+//
+//  Created by Field Employee on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct gittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
